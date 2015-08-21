5503989574d43e729819ad84eac0e92afb7cea42
NiveditN <niveditn@gmail.com>
2015-08-21 11:18:14 +0530

    code commit

---
+++ dash.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var rethinkdbdash = require('rethinkdbdash');
+
+var r = rethinkdbdash({db: 'test'});
+
+module.exports = r;
---
+++ server.js
@@ -0,0 +1,27 @@
+'user strict'
+
+var http 		= require('http'),
+	koa 		= require('koa'),
+	serve 		= require('koa-static'),
+	marko 		= require('marko'),
+	socketIo 	= require('socket.io'),
+	question 	= require('./question');
+
+var app, server, io;
+
+app = koa();
+
+app.use(serve(__dirname + '/public'));
+
+app.use(function *() {
+	this.body = marko.load('./views/index.marko').stream();
+	this.type = 'text/html';
+});
+
+server = http.Server(app.callback());
+
+io = socketIo(server);
+
+question.setup(io);
+
+server.listen(3000);
